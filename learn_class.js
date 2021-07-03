1133586859a3758e622c8a2d93aaa5aac0f10afd
milkywoosh <archer.lukman@gmail.com>
2021-07-03 23:05:03 +0700

    Inheritance for Truck
    New Inherited Class in Truck Class

--- learn_class.js
+++ learn_class.js
@@ -1,18 +1,16 @@
 class Car {
 	static fly = false;
 
-	constructor(brand, wheel, wd_type) {
+	constructor(brand = null, wheel, wd_type) {
 		this.brand = brand;
 		this.wheel = wheel;
 		this.wd_type = wd_type;
 	}
 
-	brand_name(data) {
-		this.brand = data;
+	brand_name() {
 		console.log("Car Brand: ",this.brand) ;
 	}
-	num_of_wheel(data) {
-		this.wheel = data;
+	num_of_wheel() {
 		console.log("Number of Wheel: ", this.wheel);
 	}
 	typeof_wd(data) {
@@ -39,15 +37,17 @@ Car.prototype.fuel_type = function (arg) {
 }
 
 // Tesla Brand
-const Tesla = new Car();
-Tesla.brand_name("Tesla");
-Tesla.num_of_wheel(4);
+
+const Tesla = new Car("Tesla", 4);
+Tesla.brand_name();
+Tesla.num_of_wheel();
 Tesla.typeof_wd(4);
 Tesla.can_fly();
 Tesla.shoot(true);
 Tesla.fuel_type(["Baterry", "Sun"]);
 // is this Car class ??
 console.log("Car class ??", Tesla instanceof(Car));
+console.log("\n")
 
 
 
@@ -55,11 +55,38 @@ console.log("Car class ??", Tesla instanceof(Car));
 // 				  Truck class (sub class)
 class Truck extends Car {
 	constructor(brand, wheel, container, sleep_deck) {
-		super(brand, wheel)
+		super(brand, wheel);
 		this.container = container;
 		this.sleep_deck = sleep_deck;
 	}
+	brand_name() {
+		super.brand_name();
+		// this.brand = data;;
+	}
+	num_of_wheel() {
+		super.num_of_wheel();
+	}
+	fuel_type(data) {
+		super.fuel_type(data);
+	}
+
+	have_container(data) {
+		this.container = data;
+		console.log("container: ", this.container);
+	}
+	have_sleep_deck(data) {
+		this.sleep_deck = data;
+		console.log("sleep_deck: ", this.sleep_deck);
+	}
 }
+
+const Hinno = new Truck("Hinno", 9);
+Hinno.brand_name();
+Hinno.num_of_wheel();
+Hinno.fuel_type("Solar");
+Hinno.have_container(true);
+Hinno.have_sleep_deck(false);
+console.log("Truck Class: ", Hinno instanceof(Truck));
 
 
 
