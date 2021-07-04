e2d5e21b0bc65448f8e54a5926edda9fd6e4ac34
“milkywoosh” <archer.lukman@gmail.com>
2021-07-04 21:03:02 +0700

    Add new inheritance class
    inherit CAR class for TAXI class,
    Taxi class inherit following property[brand, wheel, wd_type]
    Taxi class also inherit following methods [super.brand_name, super.num_of_wheel, super.typeof_wd],
    last method that taxi class inherit is PROTOTYPE OF fuel_type  which is created
    outside CAR class, BUT STILL PART OF CAR class

--- learn_class.js
+++ learn_class.js
@@ -1,12 +1,14 @@
 class Car {
+	// static property, 
 	static fly = false;
 
 	constructor(brand = null, wheel, wd_type) {
 		this.brand = brand;
 		this.wheel = wheel;
 		this.wd_type = wd_type;
+		
 	}
-
+	
 	brand_name() {
 		console.log("Car Brand: ",this.brand) ;
 	}
@@ -17,6 +19,7 @@ class Car {
 		this.wd_type = data;
 		console.log("wd_type: ", this.wd_type);
 	}
+	
 	can_fly() {
 		console.log ("Can fly: ", Car.fly);
 	}
@@ -38,7 +41,7 @@ Car.prototype.fuel_type = function (arg) {
 
 // Tesla Brand
 
-const Tesla = new Car("Tesla", 4);
+const Tesla = new Car("Tesla", 4, "Solar Sun");
 Tesla.brand_name();
 Tesla.num_of_wheel();
 Tesla.typeof_wd(4);
@@ -54,8 +57,8 @@ console.log("\n")
 // INHERITANCE of Car class (super class)
 // 				  Truck class (sub class)
 class Truck extends Car {
-	constructor(brand, wheel, container, sleep_deck) {
-		super(brand, wheel);
+	constructor(brand, wheel, wd_type, container, sleep_deck) {
+		super(brand, wheel, wd_type);
 		this.container = container;
 		this.sleep_deck = sleep_deck;
 	}
@@ -66,9 +69,11 @@ class Truck extends Car {
 	num_of_wheel() {
 		super.num_of_wheel();
 	}
-	fuel_type(data) {
-		super.fuel_type(data);
+	typeof_wd(data) {
+		super.typeof_wd(data);
 	}
+	
+
 
 	have_container(data) {
 		this.container = data;
@@ -80,13 +85,53 @@ class Truck extends Car {
 	}
 }
 
+
 const Hinno = new Truck("Hinno", 9);
 Hinno.brand_name();
 Hinno.num_of_wheel();
+Hinno.typeof_wd(6);
 Hinno.fuel_type("Solar");
 Hinno.have_container(true);
 Hinno.have_sleep_deck(false);
 console.log("Truck Class: ", Hinno instanceof(Truck));
+console.log("\n");
+
+
+
+class Taxi extends Car {
+	constructor(brand, wheel, wd_type, rent, num_passanger) {
+		super(brand, wheel, wd_type);
+		this.rent = rent;
+		this.num_passanger = num_passanger;
+	}
+	brand_name() {
+		super.brand_name();
+	}
+	num_of_wheel() {
+		super.num_of_wheel();
+	}
+	typeof_wd(data) {
+		super.typeof_wd(data);
+	}
+	rent_car(data) {
+		this.rent = data;
+		console.log("can rent: ", this.rent);
+	}
+	possible_passanger(data) {
+		this.num_passanger = data;
+		console.log("How many passanger: ", this.num_passanger);
+	}
+
+}
+
+const blue_bird = new Taxi("Blue Bird", 4);
+
+blue_bird.brand_name();
+blue_bird.num_of_wheel();
+blue_bird.typeof_wd(4);
+blue_bird.rent_car(true);
+blue_bird.possible_passanger(3);
+blue_bird.fuel_type("Pertalite");
 
 
 
